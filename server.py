a8ea9cb9cace4fb6860f539b4f1dc1fc17711854
Konan KOUAME <fabrice.fkouame@gmail.com>
2023-07-26 13:58:12 +0200

    del server.py file

--- server.py
+++
@@ -1,4 +0,0 @@
-from waitress import serve
-from picappAPI.wsgi import application  # Remplacez 'nom_du_projet' par le nom de votre projet Django
-
-serve(application, host='0.0.0.0', port=8000, url_scheme='https')
