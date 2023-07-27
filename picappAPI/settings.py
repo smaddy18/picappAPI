8e322309f938ae9ff72f780fd3435f02edbfef09
Konan KOUAME <fabrice.fkouame@gmail.com>
2023-07-27 14:10:03 +0200

    update settings.py file by removing the default database

--- picappAPI/settings.py
+++ picappAPI/settings.py
@@ -102,17 +102,23 @@ WSGI_APPLICATION = 'picappAPI.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-        #'ENGINE': 'django_dynamodb_engine',
-        #'NAME': 'roomInfos',
-        #'REGION_NAME': 'us-east-1',
-        #'AWS_ACCESS_KEY_ID': 'AKIARN3GXU7QATSAZZP3',
-        #'AWS_SECRET_ACCESS_KEY': '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',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'your_db_name', 
+#         'USER': 'postgres',
+#         'PASSWORD': 'your_db_password',
+#         'HOST': '127.0.0.1', 
+#         'PORT': '5432',
+#     }
+# }
 
 
 # Password validation
